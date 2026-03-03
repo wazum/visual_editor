206c94a739f1efc594dcb89bd85b5748f04f91ed
Matthias Vogel <m.vogel@andersundsehr.com>
2026-03-03 16:59:09 +0100

    [FEATURE] Implement scoped CSS loading for CKEditor in ve-editable-rich-text
    The CSS files from the RTE configuration are loaded and scoped into the frontend.
    
    However, this method is still somewhat finicky to use,
    because the normal front-end CSS is still used for the RTE.
    However, it is the best solution for now.

--- Resources/Public/JavaScript/Frontend/components/ve-editable-rich-text.js
+++ Resources/Public/JavaScript/Frontend/components/ve-editable-rich-text.js
@@ -2,12 +2,15 @@ import {LitElement} from 'lit';
 import {ClassicEditor as Editor} from '@ckeditor/ckeditor5-editor-classic';
 // import {InlineEditor as Editor} from '@ckeditor/ckeditor5-editor-inline'; // TODO fix issues with inline editor
 import {initCKEditorInstance} from '@typo3/rte-ckeditor/init-ckeditor-instance.js';
+import {prefixAndRebaseCss} from '@typo3/rte-ckeditor/css-prefixer.js';
 import {removeRuleBySelector} from '@typo3/visual-editor/Shared/remove-rule-by-selector';
 import {dataHandlerStore} from '@typo3/visual-editor/Frontend/stores/data-handler-store';
 import {showEmptyActive} from '@typo3/visual-editor/Shared/local-stores';
 import {dragInProgressStore} from '@typo3/visual-editor/Frontend/stores/drag-store';
 
 /**
+ * Styles are in editable.css
+ *
  * @extends {HTMLElement}
  */
 export class VeEditableRichText extends LitElement {
@@ -60,7 +63,10 @@ export class VeEditableRichText extends LitElement {
       wrapper.appendChild(element.firstChild);
     }
     element.appendChild(wrapper);
-    this.editor = await initCKEditorInstance(this.options || {}, element.firstElementChild, element.firstElementChild, Editor);
+
+    this.includeCssScoped(this.options.contentsCss);
+
+    this.editor = await initCKEditorInstance(this.options || {}, wrapper, wrapper, Editor);
     this.editor.editing.view.document.getRoot('main').placeholder = this.placeholder;
     this.editor.model.document.on('change:data', () => {
       this.value = this.editor.getData({ skipListItemIds: true });
@@ -73,6 +79,7 @@ export class VeEditableRichText extends LitElement {
     // reset CSS
     removeRuleBySelector('.ck.ck-editor__editable_inline > :first-child');
     removeRuleBySelector('.ck.ck-editor__editable_inline > :last-child');
+    removeRuleBySelector('.ck-content');
   }
 
   updated(changedProperties) {
@@ -89,9 +96,56 @@ export class VeEditableRichText extends LitElement {
   }
 
   /**
-   * Styles are in editable.css
-   *
+   * @param {string[]} contentsCss
    */
+  async includeCssScoped(contentsCss) {
+    if (!contentsCss) {
+      return;
+    }
+    // set id to this if not already present
+    if (!this.dataset.cssHash) {
+      // hash of contentsCss using SubtleCrypto.digest()
+      this.dataset.cssHash = 've-' + await this.hash(contentsCss.join(','));
+    }
+
+    // skip if there already is a stylesheet with this id
+    if (document.adoptedStyleSheets.some(sheet => sheet.cssHash === this.dataset.cssHash)) {
+      return;
+    }
+
+    let completeCss = '';
+
+    const scopedSheet = new CSSStyleSheet();
+    scopedSheet.name = `Scoped styles for ${this.dataset.cssHash}`;
+    scopedSheet.cssHash = this.dataset.cssHash;
+    scopedSheet.replaceSync(completeCss);
+    document.adoptedStyleSheets = [...document.adoptedStyleSheets, scopedSheet];
+
+    const promisesArray = contentsCss.map(async url => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Status ' + response.status);
+        }
+        const cssContent = await response.text();
+        const cssPrefixed = prefixAndRebaseCss(cssContent, url, [`[data-css-hash=${this.dataset.cssHash}]`]);
+        completeCss += cssPrefixed;
+        scopedSheet.replaceSync(completeCss);
+      } catch (error) {
+        console.error(`Failed to fetch CSS content for CKEditor 5 prefixing: "${url}"`, error);
+      }
+    });
+
+    await Promise.allSettled(promisesArray);
+  }
+
+  /**
+   * @param {string} input
+   * @return {Promise<string>}
+   */
+  async hash(input) {
+    return new Uint8Array(await crypto.subtle.digest('SHA-1', new TextEncoder().encode(input))).toHex();
+  }
 }
 
 customElements.define('ve-editable-rich-text', VeEditableRichText);
