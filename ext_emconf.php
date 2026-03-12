f70532244d55a96eed5abdd571e3411b47f0b1ea
Matthias Vogel <m.vogel@andersundsehr.com>
2026-03-12 09:16:45 +0100

    [TASK] set version to 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,16 +3,16 @@
 /** @var string $_EXTKEY */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 CMS Visual Editor',
-    'description' => 'brings a modern WYSIWYG editing experience to TYPO3 CMS.',
+    'description' => 'Brings a modern WYSIWYG editing experience to TYPO3 CMS.',
     'category' => 'be',
     'author' => 'Matthis Vogel',
     'author_email' => 'm.vogel@andersundsehr.com',
     'state' => 'stable',
-    'version' => '14.0.1',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'php' => '8.2.0-8.5.99',
-            'typo3' => '14.0.0-14.4.99',
+            'typo3' => '13.0.0-14.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
